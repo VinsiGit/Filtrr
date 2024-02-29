c27016d76371383772308000b9ac318bb91a7db2
Bazaar99 <113721003+Bazaar99@users.noreply.github.com>
2024-03-01 00:46:09 +0100

    fix acces token in build.sh

--- frontend/build.sh
+++ frontend/build.sh
@@ -1,3 +1,3 @@
-docker login --username bazaaer --password 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 ghcr.io
+docker login --username bazaaer --password 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 ghcr.io
 docker build . -t ghcr.io/bazaaer/filtrr-frontend:latest
 docker push ghcr.io/bazaaer/filtrr-frontend:latest
