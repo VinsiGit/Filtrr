f2c0c8b0bf5828aefa09e7be3002dfa8242a0af5
SolidSalad <grimmieboy17@gmail.com>
2024-03-04 16:09:13 +0100

    routerlink integration

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -6,27 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  navlinks = [
-    { 
-      name: "dashboard",
-      iconlink: "",  
-      url: "/dashboard" 
-    },
-    { 
-      name: "paste-in",
-      iconlink: "",  
-      url: "/pastein" 
-    },
-    { 
-      name: "settings",
-      iconlink: "",  
-      url: "/settings" 
-    },
-    { 
-      name: "model retrain",
-      iconlink: "",  
-      url: "/retrain" 
-    }
-  ]
-
 }
