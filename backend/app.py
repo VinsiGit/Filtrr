97a01d85ebb967713db66dcf22cb9e1d9acf41b7
Lander Van der Stighelen <lander@vanderstighelen.net>
2024-03-14 14:30:48 +0100

    add update rating endpoint

--- backend/app.py
+++ backend/app.py
@@ -8,9 +8,10 @@ import random
 import datetime
 import time
 import hashlib
+from flask import jsonify
 
 # Get the MongoDB connection details from environment variables
-mongo_host = os.environ.get('MONGO_HOST', 'db') # 'db' is the default name of the MongoDB service within the Docker network TODO: change to localhost for local development 
+mongo_host = os.environ.get('MONGO_HOST', 'localhost') # 'db' is the default name of the MongoDB service within the Docker network TODO: change to localhost for local development 
 mongo_port = int(os.environ.get('MONGO_PORT', '27017'))
 mongo_username = os.environ.get('MONGO_USERNAME', 'root')
 mongo_password = os.environ.get('MONGO_PASSWORD', 'mongo')
@@ -20,6 +21,8 @@ client = MongoClient(host=mongo_host, port=mongo_port, username=mongo_username, 
 
 # Get the MongoDB database
 db = client['filtrr_db']
+
+
 
 app = Flask(__name__)
 CORS(app)
@@ -54,6 +57,9 @@ def site():
 
 @app.route('/api', methods=['POST'])
 def add_site():
+    if request.content_type != 'application/json':
+        return jsonify({"error": "Unsupported Media Type"}), 415
+
     # Get the data from the request
     data = request.json
 
@@ -90,12 +96,30 @@ def add_site():
    }
 
     # Add the data to the database
-    db.mails.insert_one(response)
+    db.mails.insert_one(response.copy())
 
     # Remove the _id key from MongoDB'
-    response.pop('_id', None)
+    response.pop('id', None)
 
-    return response
+    return jsonify(response), 200
+
+
+@app.route('/api/rating', methods=['POST'])
+def update_rating():
+    data = request.json
+
+    # Add the source to the data
+    hash = {"id": str(hash_input(data['body']))}
+    new_rating = data['rating']
+
+    result = db.mails.update_one(hash, {"$set": {"rating": new_rating}})
+
+    if result.modified_count > 0:
+        return "Rating updated successfully."
+    else:
+        return "No documents matched the query. No update was made."
+    
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
